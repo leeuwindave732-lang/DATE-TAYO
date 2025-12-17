6ccbbfa6c5b723af3b9f239b7dac3fd4b1dd4d46
leeuwindave732-lang <leeuwindave732@gmail.com>
2025-12-17 19:08:53 +0800

    vercel deploymeny

--- src/pages/MapPage.tsx
+++ src/pages/MapPage.tsx
@@ -44,7 +44,7 @@ const MapPage: React.FC = () => {
                 className="h-full w-full"
             >
                 <TileLayer
-                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                        url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
                         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 />
                 {locations.map((user) => (
