5f994f27a2125d354e6ba8f6f428ce8e41ff23f3
leeuwindave732-lang <leeuwindave732@gmail.com>
2025-12-17 18:46:45 +0800

    vercel deployment

--- src/leafletFix.ts
+++ src/leafletFix.ts
@@ -1,11 +1,12 @@
 import * as L from "leaflet";
 import iconUrl from "leaflet/dist/images/marker-icon.png";
+import iconRetinaUrl from "leaflet/dist/images/marker-icon-2x.png";
 import shadowUrl from "leaflet/dist/images/marker-shadow.png";
 
 delete (L.Icon.Default.prototype as any)._getIconUrl;
 
 L.Icon.Default.mergeOptions({
+    iconRetinaUrl,
     iconUrl,
     shadowUrl,
-    iconRetinaUrl: iconUrl,
 });
